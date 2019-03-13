a93603fa6ed8136495642b73c8e55d9853626e4d
Nicole C <nicas@sonic.net>
2019-03-12 23:06:00 -0700

    Final push # 18

--- main.cpp
+++ main.cpp
@@ -33,27 +33,40 @@ int main() {
 
     const int dieSides = 6;
     int compTotal, playerTotal, compScore, playerScore;
-    char userDesision;
+    char userDecision;
     bool playRound = true;
 
+    // Initialise number of games won
     compScore = 0;
     playerScore = 0;
 
+    // Create 2 dice using Die.h and Die.cpp
+    // .roll() and .getValue() are from that sample code that are used throughout this program
     Die die1(dieSides);
     Die die2(dieSides);
 
+    // Explains the rules
+    cout << "Welcome to Blackjack Dice! The object of the game is to get the highest score by rolling" << endl;
+    cout << "6-sided die without going over 21. You are competing against the computer. If the computer" << endl;
+    cout << "happens to over 21 first, you win!" << endl << endl;
+
+    // This while-loop allows the user to play multiple rounds
     while (playRound)
     {
-        die1.roll();
-        die2.roll();
-        compTotal = die1.getValue() + die2.getValue();
+        // Reset round totals
+        compTotal = 0;
+        playerTotal = 0;
 
+        // This generates the computer's total. To make it more difficult, and possible for the computer to go over 21,
+        // it always rolls until it passes 17
         while (compTotal <= 17)
         {
             die1.roll();
-            compTotal = compTotal + die1.getValue();
+            die2.roll();
+            compTotal = compTotal + die1.getValue() + die2.getValue();
         }
 
+        // Player's initial roll
         die1.roll();
         die2.roll();
         playerTotal = die1.getValue() + die2.getValue();
@@ -61,91 +74,119 @@ int main() {
         cout << "You rolled " << die1.getValue() << " & " << die2.getValue() << endl;
         cout << "Your total is: " << playerTotal << endl;
 
-        userDesision = 'a';
+        // "Clear" userDecision so the code within while-loop isn't accidentally skipped
+        userDecision = 'a';
 
-        while (userDesision != 'y' && userDesision != 'n')
+        // Only accept 'y' or 'n' input
+        while (userDecision != 'y' && userDecision != 'n')
         {
             cout << "\nWould you like to roll again? y/n" << endl;
-            cin >> userDesision;
+            cin >> userDecision;
         }
 
-        while (userDesision == 'y')
+        // While the player wants to continue to roll
+        while (userDecision == 'y')
         {
-            if (playerTotal <= 21)
+            // Roll again
+            die1.roll();
+            die2.roll();
+            playerTotal = playerTotal + die1.getValue() + die2.getValue();
+
+            // Catch if player goes over 21
+            if (playerTotal < 21)
             {
-                die1.roll();
-                die2.roll();
-                playerTotal = playerTotal + die1.getValue() + die2.getValue();
+                // If the player has a total below 21 continue the round
 
                 cout << "You rolled " << die1.getValue() << " & " << die2.getValue() << endl;
                 cout << "Your total is: " << playerTotal << endl;
 
-                userDesision = 'a';
+                // "Clear" userDecision so the code within while-loop isn't accidentally skipped
+                userDecision = 'a';
 
-                while (userDesision != 'y' && userDesision != 'n')
+                // Only accept 'y' or 'n' input
+                while (userDecision != 'y' && userDecision != 'n')
                 {
                     cout << "\nWould you like to roll again? y/n" << endl;
-                    cin >> userDesision;
+                    cin >> userDecision;
                 }
 
             } else {
 
-                userDesision = 'n';
+                // If the player has a total at or above 21 end the round
+
+                userDecision = 'n';
             }
 
         }
 
+        // Print totals
+        cout << "\nYour final score was " << playerTotal << endl;
         cout << "The computer's score was " << compTotal << endl;
         cout << endl;
 
+        // Calculate Winner
         if (compTotal > 21)
         {
             cout << "You win! The computer went over 21" << endl;
             playerScore ++;
 
         } else if (playerTotal > 21) {
+
             cout << "The computer wins! You went over 21" << endl;
             compScore ++;
 
         } else if (playerTotal > compTotal) {
+
             cout << "You win! You beat the computer's score" << endl;
             playerScore ++;
 
         } else if (compTotal > playerTotal) {
+
             cout << "The computer wins! It beat your score" << endl;
             compScore ++;
 
         } else if (compTotal == playerTotal) {
+
             cout << "It's a tie!" << endl;
 
         } else {
+
+            // This should not be able to trigger
             cout << "Error: Something went wrong" << endl;
 
         }
 
-        cout << "You have won" << playerScore << " games and the computer has won " << compScore << endl;
+        // Display who is winning more games
+        cout << "You have won " << playerScore << " game(s) and the computer has won " << compScore << endl;
 
-        userDesision = 'a';
+        // "Clear" userDecision so the code within while-loop isn't accidentally skipped
+        userDecision = 'a';
 
-        while (userDesision != 'y' && userDesision != 'n')
+        // Only accept 'y' or 'n' input
+        while (userDecision != 'y' && userDecision != 'n')
         {
+            // User can play another round or end game
+
             cout << endl;
             cout << "\nWould you like to play another round? y/n" << endl;
-            cin >> userDesision;
+            cin >> userDecision;
         }
 
-        if (userDesision == 'n')
+        // End game
+        if (userDecision == 'n')
         {
             playRound = false;
 
         }
 
+
+
+
+        //Chapter 14 Programming Challenge #14 Parking Ticket Sim Pg 889
+
+
+
     }
-
-
-
-    //Chapter 14 Programming Challenge #14 Parking Ticket Sim Pg 889
-
 
     return 0;
 }
